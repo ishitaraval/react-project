a74af3dea867db866138480f0fcb7734dab50a04
ishraval04 <70752809+ishraval04@users.noreply.github.com>
2021-07-28 13:10:53 +0530

    changes

--- src/components/script.js
+++ src/components/script.js
@@ -1,32 +1,3 @@
-const filt_toggle = document.querySelector(`.filt-toggle`)
-const filters = document.querySelector(`.filters`)
-
-try {
-  filt_toggle.addEventListener('click',function () {
-    if (filters.classList.contains(`m-hide`)) {
-      filters.classList.remove('m-hide')
-    } else {
-      filters.classList.add('m-hide')
-    }
-  })
-}
-catch(err) {
-  console.log("Error message: "+err.message)
-}
-
-//  Toggle the product filters to show when button is click
-const nav_toggle = document.querySelector(`.nav-toggle`)
-const navigation = document.querySelector(`.navigation`)
-nav_toggle.addEventListener('click',function () {
-  if (navigation.classList.contains(`m-hide`)) {
-    navigation.classList.remove('m-hide')
-    navigation.classList.add('nav_style')
-  } else {
-    navigation.classList.remove('nav_style')
-    navigation.classList.add('m-hide')
-  }
-})
-
 const productAr =[
     {
        iprodt_img: 'iphone-x.jpg',
