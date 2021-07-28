1321df75af791133d7b77b0a58a4e15f20b2e54e
ishraval04 <70752809+ishraval04@users.noreply.github.com>
2021-07-29 01:58:00 +0530

    chges

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import 'css/reset.css';
-import 'css/index.css';
+import 'css/style.css';
 
 console.log("Welcome")
 ReactDOM.render(<App />, document.getElementById('root'))
