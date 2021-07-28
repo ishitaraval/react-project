0ba232a7519f3249d531d0f13821286daf03f198
ishraval04 <70752809+ishraval04@users.noreply.github.com>
2021-07-29 01:12:48 +0530

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     resolve: {
         alias: {
             components: path.resolve(__dirname, 'src/components/'),
-			products: path.resolve(__dirname, 'src/pages/'),
+			pages: path.resolve(__dirname, 'src/pages/'),
             css: path.resolve(__dirname, 'src/css/'),
             img: path.resolve(__dirname, 'src/img/'),
         }
