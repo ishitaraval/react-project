2d03802934e1a56e59b42946950d99e3e6cb8ccb
ishraval04 <70752809+ishraval04@users.noreply.github.com>
2021-07-29 01:56:37 +0530

    chnages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import 'css/reset.css';
-import 'css/style.css';
+import 'css/index.css';
 
 console.log("Welcome")
 ReactDOM.render(<App />, document.getElementById('root'))
