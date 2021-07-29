f81ef9544698d00f56877c079b55df43e72581da
Ishita Raval <79472935+ishitaraval@users.noreply.github.com>
2021-07-29 14:21:34 +0530

    Add files via upload

---
+++ src/components/ProductRow.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import Image from 'components/Image'
+
+
+const ProductRow = ({data}) => {
+  const {id, iprod_img, iprod_link, iprod_name, iprod_descrip, iprod_price, iprod_rating} = data
+
+    const article_pro = document.createElement('article')
+    article_pro.classList.add('product')
+    
+
+    let the_r = []
+    for (let x = 0; x < 5; x++) {
+        if (iprod_rating > x) {
+            the_r.push(<span className="material-icons">star</span>)
+        } else {
+            the_r.push(<span className="material-icons">star_border</span>)
+        }
+    }
+
+    const the_s = prod_storage.map((st) => <li><label><input type="radio" name="storage" value="s" key={id}/>{st}gb</label></li>)
+    const the_c = prod_col.map((col) => <li><label><input type="radio" name="color" value="c"/>{col}</label></li>)
+
+
+
+    
+    return (
+        <article className="products">
+            <Image src={iprod_img} alt= {`${iprod_name}`}/>
+            
+            <div className="product-prop">
+               <h3 id="filtername"><a href={iprod_link}>{iprod_name}</a></h3>
+              <data value={iprod_price[1]}><del>${iprod_price[0]}</del> <ins>${iprod_price[1]}</ins></data>
+              <p>"{iprod_descrip}."</p>
+              <dl>
+                 <dt>Rating</dt>
+                     <dd>${iprod_rating} <span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star</span><span className="material-icons">star_half</span></dd>
+                 </dl>
+              <a href="#"style={{color: 'blue', textDecoration: 'underline'}}>see more</a>
+  
+             <form className="product-prop">
+                 <fieldset>
+                     <legend>Colours</legend>
+                     <ul className= "radio-btn" id = "filter-color">
+                          ${the_c}
+                     </ul>
+                 </fieldset>
+                 <fieldset>
+                     <legend>Storage</legend>
+                     <ol className= "radio-btn" id="filter-storage">
+                          ${the_s}
+                      </ol>
+                 </fieldset>
+             </form>
+              <div className="product-list-cart">
+                  <button type="button" id="addtocart" value="addtocart"><span className="material-icons">add_shopping_cart</span> Add to Cart</button>
+                 <button type="button" id="fav" value="fav"><span className="material-icons">favorite</span></button>
+               </div>
+           </div>
+        </article>
+    )
+}
+
+export default ProductRow
---
+++ src/components/SearchResults.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+import ProductRow from 'components/ProductRow'
+import { Pagination } from 'antd'
+import 'antd/dist/antd.css'
+
+
+
+
+const SearchResults = ({result}) => {
+
+
+  const [currPage, setCurrPage] = useState(1)
+  const pageSize = 6
+  const startRow = (currPage - 1) * pageSize
+  const endRow = startRow + pageSize
+  const totalPages = Math.ceil(result.length / pageSize)
+
+
+    const theProducts = result.slice(startRow,endRow).map((product) => <ProductRow key={product.id} data={product} />)
+    
+    const updatePage = (page) => {
+      if (page < 0) 
+        setCurrPage(1)
+      else if (page > totalPages) 
+        setCurrPage(totalPages)
+      else 
+        setCurrPage(page)
+    }
+
+  return (
+    <section  id="productTable" className="results">
+        <h2 className="subheading">Results</h2>
+
+        <div id="iproducts">
+          {theProducts}
+        </div>
+
+        
+         <p id="numStudents" style={{backgroundColor: `lightgreen`}}>
+           <button onClick={()=> updatePage(currPage-1)} disabled={(currPage === 1 )?`disabled`:``}>
+             <span className="material-icons">navigate_before</span>
+           </button>
+            Showing {(theProducts.length === 1) ? `product` : `products`} {startRow + 1} to {Math.min(endRow, result.length)} of {result.length}
+           <button onClick={()=> updatePage(currPage+1)} disabled={(currPage === totalPages )?`disabled`:``} >
+             <span className="material-icons">navigate_next</span>
+           </button>
+         </p>
+         <p className="numpage">
+           <Pagination defaultCurrent={currPage} total={result.length} defaultPageSize={pageSize} onChange={(page) => setCurrPage(page)}/>
+         </p>
+        
+    </section>
+  )
+}
+
+export default SearchResults
