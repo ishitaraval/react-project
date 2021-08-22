daab1b8abb1ae3260d6bf04877eecfe8b16c8677
Ishita Raval <79472935+ishitaraval@users.noreply.github.com>
2021-08-22 22:37:13 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+import 'css/reset.css';
+import 'css/index.css';
+
+
+console.log("iphone shopping")
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
