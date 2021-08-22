45b07c179a38413836fa79d19846b44cf0d181bb
Ishita Raval <79472935+ishitaraval@users.noreply.github.com>
2021-08-22 14:40:11 +0530

    Delete products.js

--- src/context/products.js
+++
@@ -1,9 +0,0 @@
-const {useState, useContext, createContext} = React
-const {BrowserRouter, Switch, Route, Redirect, Link} = ReactRouterDOM
-
-
-//////////////////////////////////////
-// FILE: contexts/products.js
-const ProductsContext = createContext()
-
-export default ProductsContext
