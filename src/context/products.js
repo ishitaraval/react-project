19fa2d5ea60e46a72c74700116078cd8534277e8
Ishita Raval <79472935+ishitaraval@users.noreply.github.com>
2021-08-22 22:39:01 +0530

    Delete products.js

--- src/context/products.js
+++
@@ -1,9 +0,0 @@
-import {createContext} from 'react'
-
-
-
-//////////////////////////////////////
-// FILE: contexts/products.js
-const ProductsContext = createContext()
-
-export default ProductsContext
