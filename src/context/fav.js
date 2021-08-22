1bca9aa8c1895e6ac5cd4809701d8f5378ad5662
Ishita Raval <79472935+ishitaraval@users.noreply.github.com>
2021-08-22 14:40:32 +0530

    Add files via upload

---
+++ src/context/fav.js
@@ -0,0 +1,9 @@
+import {createContext} from 'react'
+ 
+
+const FavContext = createContext()
+const userMon = (cents) => {
+	return `$${(cents).toFixed(2)}`
+} 
+
+export {FavContext, userMon}
---
+++ src/context/products.js
@@ -0,0 +1,9 @@
+import {createContext} from 'react'
+
+
+
+//////////////////////////////////////
+// FILE: contexts/products.js
+const ProductsContext = createContext()
+
+export default ProductsContext
